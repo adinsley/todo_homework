92b950830a13f85396e53a25804d8a3b1574fb13
User <user@Users-MacBook-Pro-7.local>
2015-12-02 17:56:26 +0000

    Mostly completed Homework

---
+++ main.rb
@@ -0,0 +1,69 @@
+require 'sinatra'
+require 'sinatra/contrib/all' if development?
+require 'pry-byebug'
+require 'pg'
+
+get '/tasks' do
+  sql = "SELECT * FROM tasks"
+  @tasks = run_sql(sql)
+  erb :index
+end
+
+get '/tasks/new' do
+#Get a new form
+  erb :new
+end
+
+post '/tasks' do
+#Create a new task
+name = params[:name]
+details = params[:details]
+
+sql = "INSERT INTO tasks (name, details) VALUES ('#{name}', '#{details}')"
+run_sql(sql)
+redirect to ('/tasks')
+
+
+end
+
+get '/tasks/:id' do
+
+sql = "SELECT * FROM tasks WHERE tasks.id = #{params[:id]}"
+
+@task = run_sql(sql)
+
+erb :show
+end
+
+get '/tasks/:id/edit' do
+  sql = "SELECT * FROM tasks WHERE tasks.id = #{params[:id]}"
+
+  @task = run_sql(sql)
+
+  erb :edit
+end
+
+post '/tasks/:id' do
+  # Update the task id = :id
+  @name = params[:name]
+  @details = params[:details]
+  sql = "UPDATE tasks SET name = '#{@name}', details = '#{@details}' WHERE id = #{params[:id]}"
+
+
+  run_sql(sql)
+  redirect to ('/tasks')
+
+
+end
+
+post '/tasks/:id/delete' do
+  #Delete tasks
+end
+
+def run_sql(sql)
+  conn = PG.connect(dbname: 'todo', host: 'localhost')
+
+  result = conn.exec(sql)
+  conn.close
+  result
+end
